77edd0f2979f1b5a2ea946861703bd71f1633944
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-27 08:44:33 +0100

    new updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,24 +4,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-      },
-      textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
-      },
-      backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
-      },
-      borderColor: {
-        default: "var(--color-border)",
-      },
     },
   },
   corePlugins: {
