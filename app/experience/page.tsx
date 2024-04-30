2a2ac85b7577d0e10b577f2a79a79df837e34384
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-04-30 20:13:52 +0530

    feat: experience page setup

---
+++ app/experience/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Experience",
+  description: "Explore my experience.",
+};
+
+export default function ExperiencePage() {
+  return (
+      <section>
+          <p></p>
+    </section>
+  );
+}
