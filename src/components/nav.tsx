9adae8b9f3a14bec4681e985ab5e41c3a70d0411
Arjun Aditya <meow@nermalcat69.dev>
2024-09-08 11:30:43 +0530

    yayy

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,32 +5,48 @@ interface NavItem {
 }
 
 export function Nav() {
-    const currentPath = window.location.pathname; // Get current path
+    const currentPath = window.location.pathname;
 
     const items: NavItem[] = [
-        { label: "Guestbook", href: "/", icon: "/icon.svg" },
-        { href: "/another-page", icon: "/icon.svg" },
-        { label: "nermal", href: "/nermal", icon: "/icon.svg" },
-        { href: "/some-other-page", icon: "/icon.svg" },
+        { label: "guests", href: "/guestbook", icon: "/guestbook.svg" },
+        { label: "exp", href: "/experience", icon: "/experience.svg" },
+        { label: "me", href: "/me", icon: "/profile.svg" },
+        { label: "blog",href: "/blog", icon: "/blog.svg" },
     ];
 
     return (
-        <div className="absolute top-20 rotate-12 -right-20 hover:right-0 duration-[600ms] mx-6 flex justify-center rounded-full bg-neutral-900 border border-[#392A32] rectangle items-center">
-            {items.map((item, index) => {
-                const isActive = currentPath === item.href;
-                return (
-                    <a
-                        key={index}
-                        href={item.href}
-                        className={`text-[#EEEEEE] navbtn ${isActive ? 'bg-neutral-800' : 'hover:bg-neutral-800'} border-r border-[#392A32] px-4 py-2.5 flex items-center ${index === 0 ? "pl-6 rounded-l-full" : ""
-                            } ${index === items.length - 1 ? "pr-6 rounded-r-full" : ""
-                            }`}
-                    >
-                        <img src={item.icon} alt={`${item.label || "icon"}`} className={`w-4 h-4 mr-2 ${isActive ? 'text-neutral-800' : ''}`} />
-                        {item.label && <span>{item.label}</span>}
-                    </a>
-                );
-            })}
-        </div>
+        <>
+            <div className="absolute top-20 hover:rotate-0 rotate-12 -right-20 hover:right-0 duration-[600ms] mx-6 flex justify-center rounded-full bg-neutral-900 border border-[#392A32]  items-center">
+                {items.map((item, index) => {
+                    const isActive = currentPath === item.href;
+                    return (
+                        <a
+                            key={index}
+                            href={item.href}
+                            className={`text-[#EEEEEE] navbtn ${isActive ? 'bg-neutral-800' : 'hover:bg-neutral-800'} border-r border-[#392A32] px-4 py-2.5 flex items-center ${index === 0 ? "pl-6 rounded-l-full" : ""
+                                } ${index === items.length - 1 ? "pr-6 rounded-r-full" : ""
+                                }`}
+                        >
+                            <img src={item.icon} alt={`${item.label || "icon"}`} className={`w-4 h-4 mr-2 ${isActive ? 'text-neutral-800' : ''}`} />
+                            {item.label && <span className="text-[#eee]">{item.label}</span>}
+                        </a>
+                    );
+                })}
+            </div>
+            <div className="absolute top-20 -rotate-12 hover:rotate-0 -left-10 hover:-left-3 duration-[600ms] mx-6 flex justify-center rounded-full bg-neutral-900 border border-[#392A32] items-center">
+                <a
+                    href="/"
+                    className={`text-[#EEEEEE] ${currentPath === '/' ? 'border border-[#D01B68]' : 'hover:bg-neutral-800'
+                        } border-r border-[#392A32] px-4 py-2.5 flex items-center rounded-full`}
+                >
+                    <img
+                        src="/home.svg"
+                        alt="icon"
+                        className={`w-4 h-4 mr-2 ${currentPath === '/' ? 'text-neutral-800' : ''}`}
+                    />
+                    <span className="text-[#eee]">home</span>
+                </a>
+            </div>
+        </>
     );
 }
---
+++ src/routes/experience.tsx
@@ -0,0 +1,50 @@
+import "../App.css";
+import { Levitate } from "../components/levitating";
+// import { Steps } from "./components/steps";
+import WindowSize from "../components/WindowSize";
+import SomeComponent from "../components/SomeComponent";
+import { Nav } from "../components/nav";
+
+function Experience() {
+    return (
+        <div className="mx-auto max-w-2xl py-16 px-6">
+            <h2 className="ttl text-[#EAEAE7]">Experience</h2>
+            <p className="text-[#A6A49D] ttlb">Programmer / Designer</p>
+            <div className="py-16">
+                <div className="section">
+                    <h3 className="ttl">Myself</h3>
+                    <p className="txt font-thin">
+                        I love trying out new things, whether they bring a financial outcome or not.
+                    </p>
+                    <p className="txt">
+                        The point is, I enjoy almost everything from ui/ux design (despite being colorblind) to programming, marketing, writing, and finances.
+                    </p>
+                    <p className="txt">
+                        I started my programming journey in 2015 when I wanted to create my own version of Minecraft because that 90-minute free trial used to ruin my mood. Spent Some time while investing as well.
+                    </p>
+                    <p className="txt">
+                        Currently, I work as a full-stack developer at <a className="ext" href="https://zerops.io/" target="_blank">Zerops</a>, but I like to dabble in whatever comes my way.
+                    </p>
+                </div>
+                <div className="section">
+                    <h3 className="ttl">My Work Stack</h3>
+                    <p className="txt font-thin">
+                        Being blunt, I use almost everything. :3
+                    </p>
+                </div>
+                <div className="section">
+                    <h3 className="ttl">Socials</h3>
+                    <p className="txt font-thin">
+                        Checkout my <a href="https://x.com/zNermo" className="ext" target="_blank">Twitter</a>, <a href="https://github.com/nermalcat69" className="ext" target="_blank">Github</a>, <a href="https://unsplash.com/@arjunaditya" className="ext" target="_blank">Unsplash</a> and <a href="https://instagram.com/nermalcat69" className="ext" target="_blank">Instagram</a>.
+                    </p>
+                </div>
+                <SomeComponent />
+            </div>
+            <Nav />
+            <Levitate />
+            <WindowSize />
+        </div>
+    );
+}
+
+export default Experience;
