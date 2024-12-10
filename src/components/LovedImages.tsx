6221251d64f7e0437831f9ed0f9c6ae1f063a035
Arjun Aditya <meow@nermalcat69.dev>
2024-12-10 18:49:14 +0530

    Update LovedImages.tsx

--- src/components/LovedImages.tsx
+++ src/components/LovedImages.tsx
@@ -3,6 +3,7 @@ import Images from "./Images";
 const images = [
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/jurckpoqu6mkjdsol0xw",
+        webp: "Image_1.webp",
         width: 200,
         height: 200,
         alt: "Image 1",
@@ -10,6 +11,7 @@ const images = [
     },
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/bj2qxhyacuzvijj62dre",
+        webp: "Image_2.webp",
         width: 200,
         height: 200,
         alt: "Image 2",
@@ -17,6 +19,7 @@ const images = [
     },
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/dp6wqeppvoy96iz8yvhg",
+        webp: "Image_3.webp",
         width: 200,
         height: 200,
         alt: "Image 3",
@@ -24,6 +27,7 @@ const images = [
     },
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/wdv33hxli0ihsa1onpe0",
+        webp: "Image_4.webp",
         width: 200,
         height: 200,
         alt: "Image 4",
@@ -31,6 +35,7 @@ const images = [
     },
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/rf2mi9wzr9rzkx0byyjc",
+        webp: "Image_5.webp",
         width: 200,
         height: 200,
         alt: "Image 5",
@@ -38,6 +43,7 @@ const images = [
     },
     {
         src: "https://res.cloudinary.com/dyndvyaiu/image/upload/f_auto,q_auto/jxwwea8piulz4qvwyr0c",
+        webp: "Image_6.webp",
         width: 200,
         height: 200,
         alt: "Image 6",
