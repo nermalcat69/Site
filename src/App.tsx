e22c93db8048e87a3bbc4ceae2947446d3f8c92a
Arjun Aditya <meow@nermalcat69.dev>
2024-12-20 19:06:04 +0530

    testing reasons

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,25 @@ import { useState, useEffect } from 'react';
 import ServerMetrics from "./components/ServerMetrics";
 
 function App() {
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    // const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-    useEffect(() => {
-        const img = new Image();
-        img.src = '/rickyy.gif';
-        img.onload = () => {
-            setIsImageLoaded(true);
-        };
-    }, []);
+    // useEffect(() => {
+    //     const img = new Image();
+    //     img.src = '/rickyy.gif';
+    //     img.onload = () => {
+    //         setIsImageLoaded(true);
+    //     };
+    // }, []);
 
     return (
         <div className="max-w-[1100px] mx-auto pt-5">
-            {isImageLoaded && (
+            <div className="mx-auto max-w-2xl py-16 px-6 relative">
+                <div className="section">
+                    <ServerMetrics />
+                </div> 
+            </div>
+
+            {/* {isImageLoaded && (
                 <div className="absolute top-0 left-0 w-screen overflow-hidden"> 
                     <img 
                         src="/rickyy.gif" 
@@ -29,16 +35,8 @@ function App() {
                     />
                 </div>
             )}
-            {/* <div
-                className="absolute left-1/2 -translate-x-1/2 grayscale z-10 top-0 h-40 w-screen max-w-[1920px]"
-                style={{
-                    backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath stroke='%234B5EFB' stroke-width='1' d='M15 10l15 15M10 15l15-15M35 10l15 15M40 15l15-15M15 30l15 15M10 35l15-15M35 30l15 15M40 35l15-15M15 50l15 15M10 55l15-15M35 50l15 15M40 55l15-15'/%3E%3C/g%3E%3C/svg%3E")`,
-                    backgroundSize: '60px 60px',
-                    WebkitMask: 'linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0) 100%)',
-                    mask: 'linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0) 100%)'
-                }}
-            /> */}
-            <div className="mx-auto max-w-2xl py-16 px-6 relative">
+            {} */}
+            {/* <div className="mx-auto max-w-2xl py-16 px-6 relative">
                 <h2 className="text-2xl font-medium text-[#1a1a1a]">Arjun Aditya</h2>
                 <p className="text-[#a7a7a7] text-sm">Programmer / Designer</p>
                 <div className="py-6">
@@ -71,9 +69,8 @@ function App() {
                     </div>
 
                 </div>
-                {/* <Levitate /> */}
                 <WindowSize />
-            </div>
+            </div> */}
         </div>
     );
 }
