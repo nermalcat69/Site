1afc26f2f649c179aa14ef9259939eb047a50990
Arjun Aditya <meow@nermalcat69.dev>
2024-12-21 02:08:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,30 +20,28 @@ function App() {
     return (
         <div className="sm:max-w-[1100px] sm:mx-auto flex justify-center flex-col pt-5">
             {isImageLoaded && (
-                <div className="sm:absolute top-0 left-0 w-screen overflow-x-hidden"> 
+                <div className="sm:absolute top-0 left-0 w-screen overflow-hidden"> 
                     <img 
                         src="/rickyy.gif" 
                         alt="Rick"
                         draggable={false}
-                        className="w-[50px] sliding-image" 
+                        className="w-[40px] sm:w-[50px] sliding-image" 
                     />
                 </div>
             )}
-            {<div>
-            </div>}
-            <div className="mx-auto max-w-2xl py-16 px-6 relative">
-                <h2 className="text-2xl font-medium text-[#1a1a1a]">Arjun Aditya</h2>
-                <p className="text-[#a7a7a7] text-sm">Programmer / Designer</p>
-                <div className="py-6">
+            <div className="mx-auto max-w-2xl py-8 sm:py-16 px-4 sm:px-6 relative">
+                <h2 className="text-xl sm:text-2xl font-medium text-[#1a1a1a]">Arjun Aditya</h2>
+                <p className="text-xs sm:text-sm text-[#a7a7a7]">Programmer / Designer</p>
+                <div className="py-4 sm:py-6">
                     <div className="section">
                         <h3 className="ttl">Myself</h3>
-                        <p className="txt">
+                        <p className="txt text-sm sm:text-base">
                             The point is, I enjoy almost everything from ui/ux design (despite being colorblind) to programming, marketing, writing, and finances.
                         </p>
-                        <p className="txt">
+                        <p className="txt text-sm sm:text-base">
                             I started my programming journey in 2015 when I wanted to create my own version of Minecraft because that 90-minute free trial used to get on my nerves. Spent Some time in financial markets as well.
                         </p>
-                        <p className="txt">
+                        <p className="txt text-sm sm:text-base">
                             Currently, I work as a full-stack developer at <a href="https://zerops.io/" target="_blank">Zerops</a>, but I like to dabble in whatever comes my way.
                         </p>
                     </div>
